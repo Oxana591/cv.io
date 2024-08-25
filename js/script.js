2cc89520c2a278ed825d644e0d5cdfa1daae34ae
Oxana591 <103951398+Oxana591@users.noreply.github.com>
2024-08-25 21:18:14 +0300

    2

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿var i=0;
+var links=['',
+    '',
+    '',
+    '',
+    '',
+    
+]
+function next(){
+    i++;
+    if (i>9){
+        i=0;
+    }
+    else{
+        document.getElementsByClassName('slider-img')[0].src='images/'+i+'.jpg';
+    console.log(document.getElementsByClassName('slider-img').src);
+    }
+}
+function prev(){
+    i--;
+    if (i<1){
+        i=10;
+    }
+    else{
+        document.getElementsByClassName('slider-img')[0].src='images/'+i+'.jpg';
+    console.log(document.getElementsByClassName('slider-img').src);
+    }
+}
+    
